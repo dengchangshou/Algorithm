8c96a1ffe7dbcb243b87de3b519b9989106e05c4
dengchangshou <rock_dcs@163.com>
2021-09-28 18:40:35 +0800

    整数反转

---
+++ java/leetcode/ReverseInteger.java
@@ -0,0 +1,17 @@
+package algorithm.java.leetcode;
+
+//整数反转
+public class ReverseInteger {
+
+    public int reverse(int x) {
+        int y = 0;
+        while(x != 0){
+            if(y < (Integer.MIN_VALUE / 10) || y > (Integer.MAX_VALUE / 10)){
+                return 0;
+            }
+            y = y * 10 + x % 10;
+            x = x / 10;
+        }
+        return y;
+    }
+}
