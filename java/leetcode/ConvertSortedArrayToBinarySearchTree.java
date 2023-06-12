8dafc47a6aeb96e5cacd589e0de65fffc2e07f83
dengchangshou <2412445816@qq.com>
2023-06-13 01:10:20 +0800

    108. 将有序数组转换为二叉搜索树

--- java/leetcode/ConvertSortedArrayToBinarySearchTree.java
+++ java/leetcode/ConvertSortedArrayToBinarySearchTree.java
@@ -2,6 +2,9 @@ package algorithm.java.leetcode;
 
 import algorithm.java.leetcode.vo.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 
 //108. 将有序数组转换为二叉搜索树
 public class ConvertSortedArrayToBinarySearchTree {
@@ -24,4 +27,36 @@ public class ConvertSortedArrayToBinarySearchTree {
         return null;
     }
 
+    //迭代法，会超时
+    public TreeNode sortedArrayToBST1(int[] nums) {
+        if(nums.length == 0){
+            return null;
+        }
+        Queue<TreeNode> treeNodeQueue = new LinkedList<>(); //存放树的结点
+        Queue<Integer> leftIndex = new LinkedList<>(); //存放左边界的数组下标
+        Queue<Integer> rightIndex = new LinkedList<>(); //存放右边界的数组下标
+        TreeNode treeNode = new TreeNode();
+        leftIndex.offer(0);
+        rightIndex.offer(nums.length - 1);
+        treeNodeQueue.offer(treeNode);
+        int left, right, mid;
+        while(!treeNodeQueue.isEmpty()){
+            TreeNode curNode = treeNodeQueue.peek();
+            left = leftIndex.peek();
+            right = rightIndex.peek();
+            mid = left + (right - left) / 2;
+            curNode.val = nums[mid];
+            if(left < mid){
+                treeNodeQueue.offer(new TreeNode());
+                leftIndex.offer(left);
+                rightIndex.offer(mid - 1);
+            }
+            if(right > mid){
+                treeNodeQueue.offer(new TreeNode());
+                leftIndex.offer(mid + 1);
+                rightIndex.offer(right);
+            }
+        }
+        return treeNode;
+    }
 }
