b369fd83f3a732a90dad3cde921a3054e26692bc
dengchangshou <2412445816@qq.com>
2023-06-17 20:13:02 +0800

    118. 杨辉三角

---
+++ java/leetcode/PascalTriangle.java
@@ -0,0 +1,23 @@
+package algorithm.java.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//118. 杨辉三角
+public class PascalTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i = 0; i < numRows; i++){
+            List<Integer> row = new ArrayList<>();
+            for(int j = 0; j < i + 1; j++){
+                if(j == 0 || j == i){
+                    row.add(1);
+                }else {
+                    row.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+                }
+            }
+            result.add(row);
+        }
+        return result;
+    }
+}
