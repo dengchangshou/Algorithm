bd8fe48f39770f8ee335f7b8ecf06e17707cc67d
dengchangshou <2412445816@qq.com>
2023-06-25 21:37:14 +0800

    168. Excel表列名称

---
+++ java/leetcode/ExcelSheetColumnTitle.java
@@ -0,0 +1,14 @@
+package algorithm.java.leetcode;
+
+//168. Excel表列名称
+public class ExcelSheetColumnTitle {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder ans = new StringBuilder();
+        while (columnNumber > 0){
+            columnNumber--;
+            ans.insert(0, (char)('A' + columnNumber % 26));
+            columnNumber /= 26;
+        }
+        return ans.toString();
+    }
+}
