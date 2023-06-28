a0b342ea77eb8baef25bb5b937d1c6fd5b1554c1
dengchangshou <2412445816@qq.com>
2023-06-29 00:00:52 +0800

    190. 颠倒二进制位

---
+++ java/leetcode/ReverseBits.java
@@ -0,0 +1,32 @@
+package algorithm.java.leetcode;
+
+//190. 颠倒二进制位
+//这题不懂
+public class ReverseBits {
+    // you need treat n as an unsigned value
+    //位运算
+    public int reverseBits(int n) {
+        int rev = 0;
+        for (int i = 0; i < 32 && n != 0; ++i) {
+            rev |= (n & 1) << (31 - i);
+            n >>>= 1;
+        }
+        return rev;
+    }
+
+
+    //位运算分治
+    private static final int M1 = 0x55555555; // 01010101010101010101010101010101
+    private static final int M2 = 0x33333333; // 00110011001100110011001100110011
+    private static final int M4 = 0x0f0f0f0f; // 00001111000011110000111100001111
+    private static final int M8 = 0x00ff00ff; // 00000000111111110000000011111111
+
+    public int reverseBits1(int n) {
+        n = n >>> 1 & M1 | (n & M1) << 1;
+        n = n >>> 2 & M2 | (n & M2) << 2;
+        n = n >>> 4 & M4 | (n & M4) << 4;
+        n = n >>> 8 & M8 | (n & M8) << 8;
+        return n >>> 16 | n << 16;
+    }
+
+}
