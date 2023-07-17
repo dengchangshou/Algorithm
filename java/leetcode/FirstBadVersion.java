b354c35d746b5cea689abf93df967afac7e82e9e
dengchangshou <2412445816@qq.com>
2023-07-17 13:56:30 +0800

    278. 第一个错误的版本

---
+++ java/leetcode/FirstBadVersion.java
@@ -0,0 +1,21 @@
+package algorithm.java.leetcode;
+
+//278. 第一个错误的版本
+public class FirstBadVersion {
+    public int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left < right){
+            int mid = left + (right - left)/2;
+            if(isBadVersion(mid)){
+                right = mid;
+            }else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    private boolean isBadVersion(int mid) {
+        return true;
+    }
+}
